b83d662b25727241a4e098308c5ff1232bfd696a
Ebug0 <112724165+Ebug0@users.noreply.github.com>
2022-09-30 13:11:35 -0500

    Add files via upload

---
+++ Ethan_Livermore_Stats.py
@@ -0,0 +1,11 @@
+import statistics
+def meanE(number):
+    meanofnumber = statistics.mean(number)
+    return meanofnumber
+
+def median(number):
+    medofnumber = statistics.median(number)
+    return medofnumber
+
+
+
