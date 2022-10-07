1da2db48cf9b1b0d1f321d38eecb2c20779fde61
Ebug0 <112724165+Ebug0@users.noreply.github.com>
2022-10-07 12:10:58 -0500

    Add files via upload

---
+++ Ethan_Livemore_Stats2.py
@@ -0,0 +1,13 @@
+import statistics
+def takemean(variable):
+    meanoflist = statistics.mean(variable)
+    return meanoflist
+
+def takemedian (variable):
+    medianoflist = statistics.median(variable)
+    return medianoflist
+
+def takemode (variable):
+    modeoflist = statistics.mode(variable)
+    return modeoflist
+
